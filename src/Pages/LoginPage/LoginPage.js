f0ebe0bcea7100ec60295239b3652740cd42e9f5
e-sillix <sumitpanwar1807@gmail.com>
2024-01-22 15:58:49 +0530

    auth

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./LoginPage.css";
+import { auth } from "../../firebase/firebase";
+import { createUserWithEmailAndPassword, signOut } from "firebase/auth";
 
 export default function LoginPage() {
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const log = () => {
+    console.log(email.current.value);
+    console.log(password.current.value);
+  };
+
+  const signIn = async () => {
+    console.log(auth?.currentUser?.email);
+    console.log(email.current.value);
+    console.log(password.current.value);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="login_column">
@@ -11,11 +39,16 @@ export default function LoginPage() {
           <div>
             <label htmlFor="user">Email</label>
             <br />
-            <input id="user" type="text" />
+            <input id="user" type="text" ref={email} />
+            <br />
+            <label htmlFor="password">Email</label>
+            <br />
+
+            <input id="password" type="password" ref={password} />
           </div>
-          <button>Submit</button>
+          <button onClick={signIn}>Submit</button>
           <p>
-            By submiting, you agree to Amazon's Conditions of Use and Privacy
+            By submiting, you agree to Snapcart's Conditions of Use and Privacy
             Notice.
           </p>
         </div>
